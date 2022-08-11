804c1a613518c4e687313117b70548fca77ed9ba
Pavel <p_v_s@mail.ru>
2022-08-12 02:59:59 +0700

    коммит задач семинара 2 по знакомству с языками программирования

---
+++ task15/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру,
+обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.WriteLine("Введите цифру: ");
+int dig = int.Parse(Console.ReadLine()!);
+string weekday;
+int remainder;
+
+Console.Write("Указанный день недели является выходным: -> ");
+
+if (dig > 5)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
